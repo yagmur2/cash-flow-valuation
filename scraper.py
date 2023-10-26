58ea828555af5d3ee681feb259f0766b33f0c912
Yagmur <yagmur.eff1@gmail.com>
2023-10-26 10:36:11 -0500

    imports and outline

---
+++ scraper.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+import lxml
+from lxml import html
+import requests
+import numpy as np
+import pandas as pd
+
+# script to pull income statement data from HTML tags on Yahoo! Finance and store it for use in an Excel model.
+
+# select target company ticker to value
+
+# store target company ticker
+
+# find target company income statements
+
+# store enterprise value, cash flows, EBIT (net income + interest + tax), EBITDA (EBIT + depreciation + amortization), and industry
+
+# find industry peers
+
+# find industry peer multiples
+    # EV/EBITDA, EV/EBIT, P/E
+
+# find industry peer cash flows
+
+# 
