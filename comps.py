eef697560b71026b20c2c3c9777c73aaa998b6f0
Yagmur <yagmur.eff1@gmail.com>
2023-10-28 13:19:56 -0500

    functional sector and industry selection Combo boxes

--- comps.py
+++ comps.py
@@ -9,7 +9,7 @@
 # Import Tkinter for GUI, ttk for Combobox
 from tkinter import *
 from tkinter import ttk
-root = Tk()
+
 
 # Create list of sectors, and each sector's contained industries
 sectors = ['Technology', 'Financial Services', 'Consumer Cyclical', 'Healthcare', 'Industrials', 'Communication Services', 'Consumer Defensive', 'Energy', 'Basic Materials', 'Real Estate', 'Utilities']
@@ -29,12 +29,56 @@ industries = [tech_inds, fin_inds, cyclic_inds, health_inds, indust_inds, comm_i
 # Create a dictionary assigning each sector to a list of its industries
 paired = dict(zip(sectors, industries))
 
-# Create OptionMenu
-choices = sectors
-variable = StringVar(root)
+# Initialize GUI window
+secwindow = Tk()
+secwindow.title('Comps Industry')
+secwindow.geometry('500x250')
+ttk.Label(secwindow, text = 'Sector selection')
+
+# Target sector and industry variables
+target_sector = 'Technology'
+target_industry = ''
+
+# Check current value of sectorbox and assign to global variable target_sector
+def check_sbox(event):
+    global target_sector
+    target_sector = sectorbox.get()
+
+# Check current value of industrybox and assign to global variable target_industry
+def check_ibox(event):
+    global target_industry
+    target_industry = industrybox.get()
+
+def print_sector():#DEBUG
+    print(target_sector)
+
+def print_industry():#DEBUG
+    print(target_industry)
+
+# Create Combobox for sector selection
+sector_choices = sectors
+variable = StringVar(secwindow)
 variable.set('Technology')
-w = ttk.Combobox(root, values = choices)
-w.pack(); root.mainloop()
+sectorbox = ttk.Combobox(secwindow, values = sector_choices)
+sectorbox.bind('<<ComboboxSelected>>', check_sbox)
+sec_butt= Button(secwindow, text="Print sector", command=print_sector)
+sec_butt.pack()
+sectorbox.pack(); secwindow.mainloop()
+
+# Initialize GUI window
+indwindow = Tk()
+indwindow.title('Comps Industry')
+indwindow.geometry('500x250')
+ttk.Label(indwindow, text = 'Sector selection')
+
+# Create Combobox for industry selection, with choices from chosen sector's paired industries
+#industry_choices = paired[sectorbox.get()]
+industry_choices = paired[target_sector]
+industrybox = ttk.Combobox(indwindow, values = industry_choices, textvariable=variable)
+industrybox.bind('<<ComboboxSelected>>', check_ibox)
+ind_butt = Button(indwindow, text="Print industry", command=print_industry)
+ind_butt.pack()
+industrybox.pack(); indwindow.mainloop()
 
 # INPUTS
 
