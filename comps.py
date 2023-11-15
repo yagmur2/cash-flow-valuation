eb37c0ca52fb695099116fbded2ae123cd6c721f
Yagmur <yagmur.eff1@gmail.com>
2023-11-15 13:00:41 -0600

    added ticker scraping code to comps.py

--- comps.py
+++ comps.py
@@ -4,12 +4,33 @@
 # into EITHER the Excel model, the scraper script, or both. AFTER trimming peer group and
 # storing average variables, THEN run the rest of the scripts for each company.
 
-# https://finance.yahoo.com/screener/predefined/sec-ind_ind-largest-equities_software-infrastructure
-
+# -----------------------------GLOBAL VARIABLES-----------------------------
 # Import Tkinter for GUI, ttk for Combobox
 import customtkinter as ctk
 import tkinter as tk
 from tkinter import ttk
+import requests
+from bs4 import BeautifulSoup
+import json
+
+# Request headers to simulate browser request
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'close',
+    'DNT': '1', # Do Not Track Request Header 
+    'Pragma': 'no-cache',
+    'Referrer': 'https://google.com',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
+}
+
+# Once we get our list of industry tickers, we store it here
+industry_path = 'data/ind-sample.json'
+
+# The URL of the industry's top 100 Market Caps TODO: Make this dynamically change based on user selected sector/industry
+url = "https://finance.yahoo.com/screener/predefined/sec-ind_ind-largest-equities_software-infrastructure?offset=0&count=100"
 
 # Create list of sectors, and each sector's contained industries
 sectors = ['Technology', 'Financial Services', 'Consumer Cyclical', 'Healthcare', 'Industrials', 'Communication Services', 'Consumer Defensive', 'Energy', 'Basic Materials', 'Real Estate', 'Utilities']
@@ -38,7 +59,9 @@ peersize = 20
 
 # Include companies within this many std. dev. of group's median P/E ratio
 range_factor = 3
+# --------------------------------------------------------------------------
 
+# ---------------------------------FUNCTIONS--------------------------------
 # Initialize GUI window
 window = ctk.CTk()
 window.title('Comps Inputs')
@@ -94,7 +117,12 @@ def check_rangebox():
     print(range_factor)#DEBUG
     window2.destroy()
 
-#-----------------------------DEBUG ASSISTANCE----------------------------
+# TODO: Dynamically change requests URL based on selected industry
+def industryURL(x):
+    print(True)
+# -------------------------------------------------------------------------
+
+# -----------------------------DEBUG ASSISTANCE----------------------------
 def print_sector():
     print(target_sector)
 
@@ -104,9 +132,9 @@ def print_industry():
     print(target_industry)
 
 ind_butt = ctk.CTkButton(window, text="(DEBUG) Print industry", command=print_industry)
-#------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 
-#--------------------------------Industry Selection----------------------
+# --------------------------------INDUSTRY SELECTION-----------------------
 # Create Combobox for industry selection
 industrybox = ttk.Combobox(window, justify='center')
 
@@ -125,8 +153,9 @@ peerbox.insert(0, str(peersize))
 peer_butt = ctk.CTkButton(window, text = 'Confirm Inputs', command=check_peerbox)
 
 sectorbox.pack(); sec_butt.pack(); industrybox.pack(); ind_butt.pack(); peerbox.pack(); peer_butt.pack(); window.mainloop()
-#-------------------------------------------------------------------------
+# -------------------------------------------------------------------------
 
+# --------------------------------TRIMMING MODIFIERS-----------------------
 # Create 2nd window
 window2 = ctk.CTk()
 window2.title("Raw Sample Analysis")
@@ -138,18 +167,42 @@ rangebox.insert(0, str(range_factor))
 range_butt = ctk.CTkButton(window2, text = 'Confirm P/E range', command = check_rangebox)
 
 rangebox.pack(); range_butt.pack(); window2.mainloop()
+# -------------------------------------------------------------------------
 
-def industryURL():
-    global target_sector
-    global target_industry
+# --------------------------------SYMBOL RETRIEVAL-------------------------
+# Send a GET request to the url and get the response
+response = requests.get(url, headers=headers)
 
-    for c in target_sector :
-        print(True)
+# Parse the response content as HTML using BeautifulSoup
+soup = BeautifulSoup(response.content, "html.parser")
 
+# Find the table element that contains the ticker information
+table = soup.find("table", class_="W(100%)")
 
+# Find all the table rows that contain the ticker information
+rows = table.find_all("tr", class_="simpTblRow")
 
-# OUTPUTS
+# Create an empty list to store the tickers
+tickers = []
 
-# List of company tickers in ind-sample.json
+# Loop through each row and extract the ticker symbol
+for row in rows:
+    # Find the first table cell element that has the class "Va(m)"
+    td = row.find("td", class_="Va(m)")
+    # Get the text content of the td element
+    ticker = td.text
+    # Append the ticker to the list
+    tickers.append(ticker)
 
+# Print the list of tickers
+print(tickers)
+
+# Open the file in write mode
+with open(industry_path, "w") as f:
+    # Dump the list of tickers as a json array
+    json.dump(tickers, f)
+
+# Print a success message
+print(f"Saved {len(tickers)} tickers to {industry_path}")
+# ------------------------------------------------------------------------
 
