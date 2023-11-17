8a8b129b7df842d056405df71143836d47bbc61c
Yagmur <yagmur.eff1@gmail.com>
2023-11-16 22:34:27 -0600

    scraper now creates valuation for each stock in json (rudimentary)

--- scraper.py
+++ scraper.py
@@ -1,12 +1,17 @@
-### script to pull income statement data from HTML tags on (WSJ Markets? Use something with free 5+ year data) Yahoo! Finance and store it for use in valuation.
+# script to pull income statement data from HTML tags on (WSJ Markets? Use something with free 5+ year data) Yahoo! Finance and store it for use in valuation.
 
 from datetime import datetime
+import json
 import lxml
 from lxml import html
 import requests
 import numpy_financial as np
 import pandas as pd
 import yfinance as yf
+
+# Load industry ticker list generated from comps script
+f = open('data/ind-sample.json')
+ind_sample = json.load(f)
 
 # Forecast length
 years = 5
@@ -17,70 +22,69 @@ print('Short-term forecast length: ' + str(years) + ' years' + '\n')
 lg = .02
 print('Terminal growth rate: ' + str(lg) + '\n')
 
-# stock currently examined TODO: make dynamic from list in ind-sample.json
-peer = yf.Ticker("MSFT")
-price = peer.history()['Close'].iloc[-1]
+for stock in ind_sample:
 
-# WACC inputs TODO: make beta and any other retrievables dynamic
+    # stock currently examined TODO: make dynamic from list in ind-sample.json
+    peer = yf.Ticker(stock)
+    price = peer.history()['Close'].iloc[-1]
 
-# Risk-free rate
-rf = .04
-# Beta of stock
-beta = 1.1
-# Expected return of market
-rm = .1
-# Weight of equity for company
-pct_eq = .8
-# Weight of liabilities for company
-pct_debt = 1 - pct_eq
-# Cost of equity
-cost_eq = rf + beta * (rm - rf)
-# Cost of debt
-cost_debt = .1
-# Tax rate
-tax = .2
-# Weighted average cost of capital (Discount rate)
-wacc = pct_eq * cost_eq + pct_debt * cost_debt * (1 - tax)
-print('WACC/Discount rate: ' + str(wacc) + '\n')
+    # WACC inputs TODO: make beta and any other retrievables dynamic
 
-# take fcf column and reverse dates
-fcf = peer.cashflow.iloc[0]
-fcf = fcf[::-1]
-print('Free cash flows for past 5 years of ' + str(peer) + ': \n' + str(fcf) + '\n')
+    # Risk-free rate
+    rf = .04
+    # Beta of stock
+    beta = 1.1
+    # Expected return of market
+    rm = .1
+    # Weight of equity for company
+    pct_eq = .8
+    # Weight of liabilities for company
+    pct_debt = 1 - pct_eq
+    # Cost of equity
+    cost_eq = rf + beta * (rm - rf)
+    # Cost of debt
+    cost_debt = .1
+    # Tax rate
+    tax = .2
+    # Weighted average cost of capital (Discount rate)
+    wacc = pct_eq * cost_eq + pct_debt * cost_debt * (1 - tax)
+    print('WACC/Discount rate: ' + str(wacc) + '\n')
 
-# short term growth rate
-sg = fcf.pct_change()
-sg = sg.fillna(0).tolist()
-sg = sum(sg)/4
-print('Short term growth rate: ' + str(sg) + '\n')
+    # take fcf column and reverse dates
+    fcf = peer.cashflow.iloc[0]
+    fcf = fcf[::-1]
+    print('Free cash flows for past 5 years of ' + str(peer) + ': \n' + str(fcf) + '\n')
 
-# Calculate short-term cash flow forecasts
-future_fcf = [0] * years
-future_fcf[0] = fcf[-1] * (1 + sg)
-while n < years :
-    future_fcf[n] = future_fcf[n - 1] * (1 + sg)
-    n += 1
-print('Short term forecasted cash flows: ' + str(future_fcf) + '\n')
+    # short term growth rate
+    sg = fcf.pct_change()
+    sg = sg.fillna(0).tolist()
+    sg = sum(sg)/4
+    print('Short term growth rate: ' + str(sg) + '\n')
 
-# Calculate perpetuity and add to previous cash flows
-tv = (future_fcf[-1] * (1 + lg))/(wacc - lg)
-print('Terminal value: ' + str(tv) + '\n')
+    # Calculate short-term cash flow forecasts
+    future_fcf = [0] * years
+    future_fcf[0] = fcf[-1] * -(1 + sg)
+    while n < years :
+        future_fcf[n] = future_fcf[n - 1] * (1 + sg)
+        n += 1
+    print('Short term forecasted cash flows: ' + str(future_fcf) + '\n')
 
-valuation = np.npv(wacc, future_fcf) + np.pv(wacc, 1, 0, tv)
-print('Market cap valuation: ' + str(valuation) + '\n')
+    # Calculate perpetuity and add to previous cash flows
+    tv = -(future_fcf[-1] * (1 + lg))/(wacc - lg)
+    print('Terminal value: ' + str(tv) + '\n')
 
-# Create verdict
-if valuation < peer.fast_info['market_cap'] :
-    print('Overvalued')
-else : print('Undervalued')
+    valuation = np.npv(wacc, future_fcf) + tv * 1/(1 + wacc)**years
+    print('Market cap valuation: ' + str(valuation) + '\n')
+    print ('Actual market cap: ' + str(peer.fast_info['market_cap']) + '\n')
+
+    # Create verdict
+    if valuation < peer.fast_info['market_cap'] :
+        print('Overvalued\n')
+    else : print('Undervalued\n')
 
 ### store enterprise value, cash flows, EBIT (net income + interest + tax), EBITDA (EBIT + depreciation + amortization), and industry
-
-### find industry peers
 
 ### find industry peer multiples
     ## EV/EBITDA, EV/EBIT, P/E
 
-### find industry peer cash flows
-
-###
+f.close()
